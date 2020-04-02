e0f50610918396433f7d9544b1845faaf3ce6c7f
Tanakan7 <ywff34tk@gmail.com>
2020-04-02 18:44:20 +0900

    マーカー作成

--- src/components/Sample/MapSample.jsx
+++ src/components/Sample/MapSample.jsx
@@ -1,8 +1,13 @@
 import React, { useEffect, useState } from 'react'
 
+const markerClick = (e, map) => {
+  console.log(e, map)
+}
+
 export const MapSample = () => {
-  const [, setMap] = useState(null)
+  const [map, setMap] = useState(null)
   useEffect(() => {
+    console.log('useEffect Map')
     setMap(
       new window.google.maps.Map(document.getElementById('map'), {
         center: { lat: 35.675069, lng: 139.763328 },
@@ -10,6 +15,21 @@ export const MapSample = () => {
       }),
     )
   }, [])
+
+  useEffect(() => {
+    console.log('useEffect marker')
+    if (map) {
+      const marker = new window.google.maps.Marker({
+        // 35.6761 139.768382
+        position: { lat: 35.677699, lng: 139.761735 },
+        map: map,
+        title: 'Hello Istanbul!',
+      })
+      marker.addListener('click', e => {
+        markerClick(e, map)
+      })
+    }
+  }, [map])
   console.log('a')
   return <div style={{ width: '100%', height: 640 }} id="map" />
 }
