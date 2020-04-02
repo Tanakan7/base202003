239dbea0fa73e24d41b20b343360340046a70473
Tanakan7 <ywff34tk@gmail.com>
2020-04-02 19:10:27 +0900

    移動時の処理、マップ初期化時のオプション追加

--- src/components/Sample/MapSample.jsx
+++ src/components/Sample/MapSample.jsx
@@ -12,6 +12,12 @@ export const MapSample = () => {
       new window.google.maps.Map(document.getElementById('map'), {
         center: { lat: 35.675069, lng: 139.763328 },
         zoom: 15,
+        // maxZoom:
+        // minZoom:
+        streetViewControl: false,
+        fullscreenControl: false,
+        mapTypeControl: false,
+        clickableIcons: false,
       }),
     )
   }, [])
@@ -22,14 +28,20 @@ export const MapSample = () => {
       const marker = new window.google.maps.Marker({
         // 35.6761 139.768382
         position: { lat: 35.677699, lng: 139.761735 },
-        map: map,
+        map,
         title: 'Hello Istanbul!',
       })
+
+      const handleDragEnd = () => {
+        console.log('移動した', map.center.lat(), map.center.lng())
+      }
+      map.addListener('dragend', handleDragEnd)
+
       marker.addListener('click', e => {
         markerClick(e, map)
       })
     }
   }, [map])
-  console.log('a')
+  console.log('再評価(MapSample.jsx')
   return <div style={{ width: '100%', height: 640 }} id="map" />
 }
